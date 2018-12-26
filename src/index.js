6f445a801b6c8ae2ee1910555ce55fef3643ebe5
Josh Byron <joshbyron@Joshs-Air.PK5001Z>
2018-12-26 11:11:17 -0600

    links pg to project db

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
+
+// *----------* Create react *----------*
 import React from 'react';
-import ReactDOM from 'react-dom';
+import App from './components/App/App';
+
+// *----------* Redux *----------*
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
+// *----------* Router *----------*
+import ReactDOM from 'react-dom';
+
+// *----------* Reducers & Stores *----------*
 import rootReducer from './redux/reducers'; // imports ./redux/reducers/index.js
 import rootSaga from './redux/sagas'; // imports ./redux/sagas/index.js
-
-import App from './components/App/App';
 
 const sagaMiddleware = createSagaMiddleware();
 
 // this line creates an array of all of redux middleware you want to use
 // we don't want a whole ton of console logs in our production code
-// logger will only be added to your project if your in development mode
+// logger will only be added to your project if youre in development mode
 const middlewareList = process.env.NODE_ENV === 'development' ?
   [sagaMiddleware, logger] :
   [sagaMiddleware];
 
 const store = createStore(
+  // Question: Saga middleware uses the rootreducer? rootsaga contains all other reducers?
+
   // tells the saga middleware to use the rootReducer
   // rootSaga contains all of our other reducers
   rootReducer,
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -9,6 +9,6 @@ const userReducer = (state = {}, action) => {
   }
 };
 
-// user will be on the redux state at:
-// state.user
+// user will be on the redux state at: state.user
+// In 
 export default userReducer;
