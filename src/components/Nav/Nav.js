bdd6d49212d5a117fa46d2a81c04a9b7cae2b2ba
Josh Byron <joshhbyron@gmail.com>
2018-12-26 12:22:45 -0600

    Initial sanitizing of project

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,19 +4,23 @@ import { connect } from 'react-redux';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 
+ {/* Show this link if they are logged in or not,
+        but call this link 'Home' if they are logged in,
+        and call this link 'Login / Register' if they are not */}
+        {/* Show the link to the info page and the logout button if the user is logged in */}
+        {/* Always show this link since the about page is not protected */}
+        
 const Nav = (props) => (
   <div className="nav">
-    <Link to="/home">
+    {/* <Link to="/home">
       <h2 className="nav-title">Prime Solo Project</h2>
     </Link>
     <div className="nav-right">
       <Link className="nav-link" to="/home">
-        {/* Show this link if they are logged in or not,
-        but call this link 'Home' if they are logged in,
-        and call this link 'Login / Register' if they are not */}
+       
         {props.user.id ? 'Home' : 'Login / Register'}
       </Link>
-      {/* Show the link to the info page and the logout button if the user is logged in */}
+
       {props.user.id && (
         <>
           <Link className="nav-link" to="/info">
@@ -25,11 +29,11 @@ const Nav = (props) => (
           <LogOutButton className="nav-link"/>
         </>
       )}
-      {/* Always show this link since the about page is not protected */}
+      
       <Link className="nav-link" to="/about">
         About
       </Link>
-    </div>
+    </div> */}
   </div>
 );
 
