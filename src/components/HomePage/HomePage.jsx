1bd0511b7bc5d58aded89377cb8cf5198f6074e1
Josh Byron <joshbyron@Joshs-MacBook-Air.local>
2019-01-07 15:12:15 -0600

    Minor changes to theme and Nav bar

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -72,7 +72,7 @@ class HomePage extends Component {
         return (
             <div style={{ justifyContent: 'center' }}>
 
-                <p className="DisplayText"> Helix<em>.</em>GG </p>
+                <p className="DisplayText"> Helix<em>.GG</em></p>
                 <div className="searchBar">
                     <input className='searchForm' onChange={this.handleChange} value={this.state.usernameInput} type="text" placeholder="Search summoner" spellCheck="false">
                     </input>
