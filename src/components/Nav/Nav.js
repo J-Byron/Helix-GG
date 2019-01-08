db3521cf7254da8f0d75ad1bc2c6f67856ecf0ce
Josh Byron <joshhbyron@gmail.com>
2019-01-07 20:50:38 -0600

    Minor changes to nav/homepage, adds LoginButton and LoginDropDOWN

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,27 +1,59 @@
-import React from 'react';
+// *----------* React *----------*
+import React, { Component } from 'react';
+
+// *----------* Router *----------*
 import { Link } from 'react-router-dom';
+
+// *----------* Redux *----------*
 import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
+
+// *----------* Page components *----------*
+import LoginButton from '../LoginButton/LoginButton';
+import LoginDropDown from '../LoginDropDown/LoginDropDown';
+
+// import LogOutButton from '../LogOutButton/LogOutButton';
+
+// *----------* Styling *----------*
 import './Nav.css';
 
- {/* Show this link if they are logged in or not,
-        but call this link 'Home' if they are logged in,
-        and call this link 'Login / Register' if they are not */}
-        {/* Show the link to the info page and the logout button if the user is logged in */}
-        {/* Always show this link since the about page is not protected */}
-        
-const Nav = (props) => (
-  <div className="nav">
-    <Link to="/home">
-      <h2 className="nav-title"> </h2>
-    </Link>
-    <div className="nav-right">
-      <Link className="nav-link" to="/home">
-        {props.user.id ? 'Home' : 'Login'}
-        {}
-      </Link>
+//  {/* Show this link if they are logged in or not,
+//         but call this link 'Home' if they are logged in,
+//         and call this link 'Login / Register' if they are not */}
+//         {/* Show the link to the info page and the logout button if the user is logged in */}
+//         {/* Always show this link since the about page is not protected */}
 
-      {props.user.id && (
+class Nav extends Component {
+  state = {
+    expandedLogin: false,
+    expandRegister: false
+  }
+
+  toggleDropDown = () => {
+    this.setState({
+      expandedLogin: !this.state.expandedLogin
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="nav">
+
+          {this.props.user.id ? 'Home' : (<LoginButton toggleDropDown={this.toggleDropDown} />)}
+
+        </div>
+
+        <LoginDropDown isToggled={this.state.expandedLogin} />
+      </div>);
+  }
+}
+
+{/* <Link style={{textDecoration: 'none'}}className="nav-link" to="/home">
+        {props.user.id ? 'Home' : (<LoginButton/>)}
+        {}
+      </Link> */}
+
+{/* {props.user.id && (
         <>
           <Link className="nav-link" to="/info">
             Info Page
@@ -32,11 +64,7 @@ const Nav = (props) => (
       
       <Link className="nav-link" to="/about">
         About
-      </Link>
-    </div>
-  </div>
-);
-
+      </Link> */}
 // Instead of taking everything from state, we just want the user
 // object to determine if they are logged in
 // if they are logged in, we show them a few more links 
