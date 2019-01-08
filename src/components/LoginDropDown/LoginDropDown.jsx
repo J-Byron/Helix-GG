a8ace9600122fd53c20a43108f1d7853a47805fb
Josh Byron <joshbyron@Joshs-Air.PK5001Z>
2019-01-07 20:50:38 -0600

    Minor changes to nav/homepage, adds LoginButton and LoginDropDOWN

---
+++ src/components/LoginDropDown/LoginDropDown.jsx
@@ -0,0 +1,16 @@
+// *----------* Create react *----------*
+import React, {Component} from 'react';
+
+// *----------* Redux *----------*
+import {connect} from 'react-redux';
+
+// *----------* Styling *----------*
+
+// Responsible for login using in
+class LoginDropDown extends Component {
+    render(){
+        return;
+    }
+}
+
+export default LoginDropDown;
