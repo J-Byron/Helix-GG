8e54d5eee4904622dac11a3a6672608cff1b1c7e
Josh Byron <joshhbyron@gmail.com>
2019-01-23 20:41:19 -0600

    adds production build

---
+++ build/precache-manifest.605ea93cc7701e91c61bec49dbf08f1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad6d484355eb7b34a9e8",
+    "url": "/static/css/main.d9b7dd4d.chunk.css"
+  },
+  {
+    "revision": "ad6d484355eb7b34a9e8",
+    "url": "/static/js/main.ad6d4843.chunk.js"
+  },
+  {
+    "revision": "960de5070ca4ba33fd0f",
+    "url": "/static/js/1.960de507.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "504f1bd5d02696ca5ca2c2e95cf35d9c",
+    "url": "/index.html"
+  }
+];
